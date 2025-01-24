a7566baa55468e7741c94e445031198795296707
Earnest-Minds <theearnestminds@gmail.com>
2025-01-24 15:23:58 +0530

    update uri

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ if (
   delete process.env.HOST;
 }
 
-const host = new URL(process.env.SHOPIFY_APP_URL || "http://localhost")
+const host = new URL(process.env.SHOPIFY_APP_URL || "https://espira-goldprice.onrender.com")
   .hostname;
 let hmrConfig;
 
